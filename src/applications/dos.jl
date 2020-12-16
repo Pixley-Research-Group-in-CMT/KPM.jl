b1bf9036958129f968f60a7de56b24fc53be9f0b
yxfu93 <yxfu93@hotmail.com>
2020-12-16 15:55:13 -0500

    fix dos methods

--- src/applications/dos.jl
+++ src/applications/dos.jl
@@ -4,63 +4,104 @@ using ProgressBars
 """
 $(METHODLIST)
 
-Calculate DOS for a fermi energy grid spanning Erange with Ntilde total points.
-If Erange is (0,0), automatically set it to be sightly smaller than full size.
+Calculate DOS for a fermi energy grid spanning `E_range` with `N_tilde` total points.
+If `E_range` is not set,  automatically set it to be sightly smaller than full size.
+Otherwise an explicit array of `E_grid` can be passed in. Don't do both.
 
-When 
+Either a) pass in a 1d array as moment and  as normalization factor; 
+or b) pass in a Hamiltonian that is rescaled with an optional keyword
+`rescale_factor` that default to 1.
+
+- `H_rescale_factor` is the normalization of H. Needed when μ is passed.
+
+- `NR` random vectors. Needed when H is passed
+
 """
 function dos end
 
+"""
+$(METHODLIST)
+
+Calculate DOS and energy derivatives at zero energy.
+"""
+function dos0 end
 
 
-function dos(μ,
-             a::Float64,
-             Ntilde::Integer;Erange::Array{T,1}where {T<:Real}=[-1.0,1.0],
-             NC::Integer=0,
-             kernel = JacksonKernel)
-    if length(size(μ)) != 1
-        println("The input is not 1D array. Assuming the input is Hamiltonian normalized by a. Running `kpm_1d`...")
-        H = μ
-        NH = size(H)[1]
-        if NC == 0
-            println("NC is set to default (1024)")
-            NC = 1024
-        end
-        NR = 21
-        println("NR is set to default (21)") #TODO better default NR needed
-        μ = kpm_1d(H, NC, NR)
-    end
 
+function dos(
+             H;
+             NC::Int64=1024,
+             NR::Int64=12,
+             E_grid=nothing,
+             N_tilde::Int64=0,
+             E_range=nothing,
+             kernel = JacksonKernel,
+             fix_normalization = 0
+            )
+    H_rescale_factor, H_norm = normalizeH(H; fixed_a=fix_normalization)
+    μ = kpm_1d(H, NC, NR)
+
+    return dos(μ, H_rescale_factor; E_grid=E_grid, E_range=E_range, N_tilde=N_tilde, kernel=kernel, NC=NC)
+end
+
+
+function dos(
+             μ, H_rescale_factor;
+             E_grid=nothing,
+             N_tilde::Int64=0,
+             E_range=nothing,
+             NC::Int64=0,
+             kernel = JacksonKernel,
+            )
+    @assert (length(size(μ)) == 1) "The input need to be 1D array"
     μ = maybe_to_device(μ)
+    @assert H_rescale_factor > 0
+    a = H_rescale_factor # for convenience
 
-    if (Erange[1]==0 && Erange[2]==0) 
-        Erange=[-a+0.01,a-0.01]
-    end
     if (NC==0)
         NC = length(μ)
     else
         NC = min(NC,length(μ))
     end
 
-    Evals_full = collect(((0:(Ntilde)).*(Erange[2]-Erange[1]))./Ntilde .+ Erange[1])
-    rhoE_full = similar(Evals_full)
+
+    if isnothing(E_grid)
+        if N_tilde == 0
+            N_tilde = NC * 2
+            println("Setting N_tilde=$(N_tilde).")
+        end
+        if isnothing(E_range) 
+            E_range = [-a+0.01, a-0.01]
+            println("Setting E_range=$(E_range).")
+        end
+        E_grid = collect(((0:(N_tilde)).*(E_range[2]-E_range[1]))./N_tilde .+ E_range[1])
+
+    else
+        @assert isnothing(E_range) "Should not set `E_grid` and `E_range` simoutaneously."
+        @assert (length(E_grid) == N_tilde) """`N_tilde` does not match with `E_grid`.
+        `N_tilde` is only necessary when using `E_range` instead of `E_grid`"""
+    end
+
+
+
+    rhoE_full = similar(E_grid)
     rhoE_full .= 0
 
 
-    idx = (abs.(Evals_full) .< abs(a))
-    Evals = maybe_to_device(Evals_full[idx])
+    idx = (abs.(E_grid) .< abs(a))
+    E_grid_inrange = maybe_to_device(E_grid[idx])
 
     n_grid = maybe_to_device(collect(0:(NC-1)))
 
     hgn = maybe_to_device(kernel.(0:(NC-1),NC) .* hn.(0:(NC-1)))
 
-    rhoE = chebyshev_lin_trans(Evals / a,
+    rhoE = chebyshev_lin_trans(E_grid_inrange / a,
                                n_grid,
                                μ .* hgn)
 
-    denom = @. (a*pi*sqrt(1-(Evals/a)^2))
+    denom = @. (a*pi*sqrt(1-(E_grid_inrange/a)^2))
     rhoE ./= denom
 
     rhoE_full[idx] = maybe_to_host(rhoE)
-    return Evals_full, rhoE_full
+    return E_grid, rhoE_full
 end
--- test/integration_test.jl
+++ test/integration_test.jl
@@ -3,4 +3,8 @@ using Test
 using KPM
 
 @load "test_data/ops_small.jld2"
+@load "test_data/exact_dos.jld2"
+H_orig = H_norm * a
+E, dos = KPM.dos(H_orig)
+
 

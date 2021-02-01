db8144e2be283ebad74ff6e9b0fd918ea5ac9ff4
Yixing <yf160@physics.rutgers.edu>
2021-02-01 10:47:02 -0500

    fix dep; lambda=0

--- src/applications/cpge.jl
+++ src/applications/cpge.jl
@@ -24,18 +24,19 @@ function cpge(Gamma, NC, ω; beta=1000000, E_f=0.0, kernel=JacksonKernel)
 end
 
 
-function Λnmp(nmp, ω; E_f=0.0, beta=100000)
+function Λnmp(nmp, ω; E_f=0.0, beta=100000, δ=1e-5)
+    # _Eflb: for test purpose, lower bound of fermi energy integral
     # Equation 43, ω1 = -ω2 = ω. ctrl+k w*
     # If possible, try taking λ→0 analytically.
     # Any additional option added should be included as optional keyword arguments.
     n, m, p = nmp
-    f(ϵ) = Δn(ϵ; n) * gn_A(ϵ - ω; m) * gn_A(ϵ; p)/(1 + exp(beta * (ϵ - E_f)))
-    I, E = quadgk(f, -1, E_f) # numerical integration, E is error
+    f(ϵ) = Δn(ϵ; n=n) * gn_A(ϵ - ω; n=m) * gn_A(ϵ; n=p)/(1 + exp(beta * (ϵ - E_f)))
+    I, E = quadgk(f, -1+δ, E_f) # numerical integration, E is error
     return I
 end
 
 
-function gn_A(ϵ; n, λ = 0.0001)
+function gn_A(ϵ; n, λ=0)
     # Equation 36 ctrl+k j3
     numerator = 2 * exp(1im * n * acos(ϵ - λ * im)) * 1im
     denominator = sqrt(1 - (ϵ - λ * im)^2)

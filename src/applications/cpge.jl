e7a3012ada668c95ac5b3f4e8d13291c42995745
Yixing <yf160@physics.rutgers.edu>
2021-02-15 23:18:54 -0500

    temporarily use fixed quadrature

--- src/applications/cpge.jl
+++ src/applications/cpge.jl
@@ -1,6 +1,7 @@
 using QuadGK # numerical integral
+using FastGaussQuadrature # numerical integral - non adaptive
 
-function cpge(Gamma, NC, ω; beta=1000000, E_f=0.0, kernel=JacksonKernel)
+function cpge(Gamma, NC, ω; beta=Inf, E_f=0.0, kernel=JacksonKernel, δ=1e-5)
     # Equation 45, last term
     # Gamma is calculated using Hamiltonian that is
     # normalized to have energy bounded by [-1, 1]
@@ -11,14 +12,24 @@ function cpge(Gamma, NC, ω; beta=1000000, E_f=0.0, kernel=JacksonKernel)
 
     Gamma_tilde = mu3D_apply_kernel_and_h(Gamma, NC, kernel)
 
-    for n in 1:NC
-        for m in 1:NC
-            for p in 1:NC
-                # note: this loop will be parallized
-                Gamma_tilde[n, m, p] *= Λnmp([n, m, p], ω)
-            end
-        end
-    end
+    nodes, weights = gausschebyshev(NC * 8)
+    quad(f) = (
+               dot(weights, f.(nodes)),
+               nothing # THIS SHOULD BE AN ESTIMATION OF ERROR
+              )
+
+
+    Λnmp_all = map(nmp -> Λnmp(nmp, ω; δ=δ, E_f=E_f, beta=beta, quad=quad), Iterators.product(0:(NC-1), 0:(NC-1), 0:(NC-1)))
+
+    Gamma_tilde .*= Λnmp_all
+    #for n in 1:NC
+    #    for m in 1:NC
+    #        for p in 1:NC
+    #            # note: this loop will be parallized
+    #            Gamma_tilde[n, m, p] *= Λnmp([n-1, m-1, p-1], ω; δ=δ, E_f=E_f, beta=beta)
+    #        end
+    #    end
+    #end
    
     return sum(Gamma_tilde) * 1im / ω^2
 end
@@ -28,27 +39,29 @@ end
 
 - E_f : Fermi energy. Between -1 and 1 because Hamiltonian is normalized. 
 """
-function Λnmp(nmp, ω; E_f=0.0, beta=Inf, δ=1e-5)
+function Λnmp(nmp, ω; E_f=0.0, beta=Inf, δ=1e-5, λ=1e-7, quad=(f->quadgk(f, -1, 1)))
     # Equation 43, ω1 = -ω2 = ω. ctrl+k w*
     # The integral will cover [-1+δ, E_f]
     # λ should be considerably smaller than δ to ensure the value of gn match with the λ->0
     # but usually not be too small to avoid floating point error dominated by large number near 1
     # future plan: if possible, try taking λ→0 analytically.
-    λ = δ / 100
+    #λ = δ / 100
     f = fermiFunctions(E_f, beta)
 
     n, m, p = nmp
 
     # apply cutoffs
-    _gn_R(ϵ; n=n) = gn_R(ϵ; n=n, λ=λ, δ=δ) 
-    _gn_A(ϵ; n=n) = gn_A(ϵ; n=n, λ=λ, δ=δ)
+    _gn_R(ϵ; n) = gn_R(ϵ; n=n, λ=λ, δ=δ) 
+    _gn_A(ϵ; n) = gn_A(ϵ; n=n, λ=λ, δ=δ)
+    _Δn(ϵ; n) = Δn(ϵ; n=n, δ=δ)
 
-    f_rr(ϵ) = gn_R(ϵ; n=n) * gn_R(ϵ - ω; n=m) * Δn(ϵ; n=p)
-    f_ar(ϵ) = gn_R(ϵ + ω; n=n) * Δn(ϵ; n=m) * gn_A(ϵ + ω; n=p)
-    f_aa(ϵ) = Δn(ϵ; n=n) * gn_A(ϵ - ω; n=m) * gn_A(ϵ; n=p)
+    f_rr(ϵ) = _gn_R(ϵ; n=n) * _gn_R(ϵ - ω; n=m) * _Δn(ϵ; n=p)
+    f_ar(ϵ) = _gn_R(ϵ + ω; n=n) * _Δn(ϵ; n=m) * _gn_A(ϵ + ω; n=p)
+    f_aa(ϵ) = _Δn(ϵ; n=n) * _gn_A(ϵ - ω; n=m) * _gn_A(ϵ; n=p)
     Λnmp_integrand(ϵ) = (f_rr(ϵ) + f_ar(ϵ) + f_aa(ϵ)) * f(ϵ)
 
-    I, E = quadgk(Λnmp_integrand, -1+δ, E_f) # numerical integration, E is error
+    #I, E = quadgk(Λnmp_integrand, -1, E_f) # numerical integration, E is error
+    I, E = quad(Λnmp_integrand)
     return I
 end
 
@@ -75,8 +88,11 @@ function gn_R(ϵ; n, λ=1e-10, δ=1e-5)
     return numerator / denominator
 end
 
-function Δn(ϵ; n)
+function Δn(ϵ; n, δ=1e-5)
     # Equation 35 ctrl+k D*
+    if abs(1 - abs(ϵ)) < δ
+        return 0.0
+    end
     numerator = 2 * cos(n * acos(ϵ))
     denominator = pi * sqrt(1 - ϵ^2)
     return numerator / denominator

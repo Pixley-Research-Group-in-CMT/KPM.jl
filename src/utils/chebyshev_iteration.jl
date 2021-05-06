440155493900dd58f6c09b371378c880550d0dd8
yxfu93 <yxfu93@hotmail.com>
2021-05-05 22:18:38 -0400

    multithreading in iteration

--- src/utils/chebyshev_iteration.jl
+++ src/utils/chebyshev_iteration.jl
@@ -1,6 +1,9 @@
 using CUDA
 using Logging
 
+
+ASA = Array{T} where {T <: SubArray}
+
 """
 evaluate from 3 to n
 ψall[:,3] comes from ψall[:,2] and ψall[:,1]
@@ -74,19 +77,34 @@ function chebyshev_iter_single(H,
     return nothing
 end
 
+# for better multi-threading performance: avoid generating views
+function chebyshev_iter_single(H,
+                               V_pp_in::ASA,
+                               V_p_in::ASA)
+
+    Threads.@threads for i = 1:size(V_pp_in)
+        @debug "i = $i on thread $(Threads.threadid())"
+        mul!(V_pp_in[i], H, V_p_in[i], 2.0+0im, -1.0+0im)
+    end
+    return nothing
+end
 
 # num indicater ver.
 # pp, p -> out
-function chebyshev_iter_single(H, V_all, i_pp_in::Int64, i_p_in::Int64, i_out::Int64)
+function chebyshev_iter_single(H, V_all::Union{Array, SubArray, CuArray}, i_pp_in::Int64, i_p_in::Int64, i_out::Int64)
     (@view V_all[:, :, i_out]) .= (@view V_all[:, :, i_pp_in])
+    chebyshev_iter_single(H, V_all, i_out, i_p_in)
+end
+function chebyshev_iter_single(H, V_all::Array{T} where {T <: ASA}, i_pp_in::Int64, i_p_in::Int64, i_out::Int64)
+    V_all[i_out] .= V_all[i_pp_in]
     chebyshev_iter_single(H, V_all, i_out, i_p_in)
 end
 
 # SubArray and CuArray are all pointer-like
 function chebyshev_iter_single(H,
-                               V_pp_in::Union{SubArray, CuArray},
-                               V_p_in::Union{SubArray, CuArray},
-                               V_out::Union{SubArray, CuArray})
+                               V_pp_in::Union{SubArray, CuArray, ASA},
+                               V_p_in::Union{SubArray, CuArray, ASA},
+                               V_out::Union{SubArray, CuArray, ASA})
     V_out .= V_pp_in
     chebyshev_iter_single(H, V_out, V_p_in)
 end

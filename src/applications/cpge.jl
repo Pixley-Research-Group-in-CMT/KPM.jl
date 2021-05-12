38997190d7cb822157064c78ddedeff8d79db78a
Yixing <yf160@physics.rutgers.edu>
2021-05-12 15:34:16 -0400

    cpge-wip

--- src/applications/cpge.jl
+++ src/applications/cpge.jl
@@ -1,5 +1,6 @@
 using QuadGK # numerical integral
 using FastGaussQuadrature # numerical integral - non adaptive
+using Logging
 
 function cpge(Gamma, NC, ω; beta=Inf, E_f=0.0, kernel=JacksonKernel, δ=1e-5, Ω=ω/20)
     # Equation 45, last term
@@ -55,9 +56,9 @@ function Λnmp(nmp, ω₁, ω₂; E_f=0.0, beta=Inf, δ=1e-5, λ=1e-7, quad=(f->
     n, m, p = nmp
 
     # apply cutoffs
-    _gn_R(ϵ; n) = gn_R(ϵ; n=n, λ=λ, δ=δ) 
-    _gn_A(ϵ; n) = gn_A(ϵ; n=n, λ=λ, δ=δ)
-    _Δn(ϵ; n) = Δn(ϵ; n=n, δ=δ)
+    _gn_R(ϵ; n) = gn_R(ϵ, n; λ=λ, δ=δ) 
+    _gn_A(ϵ; n) = gn_A(ϵ, n; λ=λ, δ=δ)
+    _Δn(ϵ; n) = Δn(ϵ, n; δ=δ)
 
     f_rr(ϵ) = _gn_R(ϵ + ω₁ + ω₂; n=n) * _gn_R(ϵ + ω₂; n=m) * _Δn(ϵ; n=p)
     f_ar(ϵ) = _gn_R(ϵ + ω₁; n=n) * _Δn(ϵ; n=m) * _gn_A(ϵ - ω₂; n=p)
@@ -69,8 +70,47 @@ function Λnmp(nmp, ω₁, ω₂; E_f=0.0, beta=Inf, δ=1e-5, λ=1e-7, quad=(f->
     return I
 end
 
+function Λnmp_fast(NC, ω₁, ω₂; E_f=0.0, beta=Inf, δ=1e-5, λ=1e-7, N_int=NC*2)
+    ϵ_grid = maybe_to_device(collect((1:N_int .- 0.5)/(N_int/2) .- 1))
+    h = ϵ_grid[2] - ϵ_grid[1]
+    n_grid = maybe_to_device(collect(0:(NC-1))')
 
-function gn_A(ϵ; n, λ=1e-10, δ=1e-5)
+    # each of the following have size (N_int, NC)
+    _gn_R_ϵ_pω1 = gn_R.(ϵ_grid .+ ω₁, n_grid; λ=λ, δ=δ)
+    _gn_R_ϵ_pω2 = gn_R.(ϵ_grid .+ ω₂, n_grid; λ=λ, δ=δ)
+    _gn_R_ϵ_pω1_pω2 = gn_R.(ϵ_grid .+ ω₁ .+ ω₂, n_grid; λ=λ, δ=δ)
+
+    _gn_A_ϵ_mω1 = gn_A.(ϵ_grid .- ω₁, n_grid; λ=λ, δ=δ)
+    _gn_A_ϵ_mω2 = gn_A.(ϵ_grid .- ω₂, n_grid; λ=λ, δ=δ)
+    _gn_A_ϵ_mω1_mω2 = gn_A.(ϵ_grid .- ω₁ .- ω₂, n_grid; λ=λ, δ=δ)
+
+    _Δn_ϵ = Δn.(ϵ_grid, n_grid; λ=λ, δ=δ)
+    @debug "size of _Δn_ϵ is $(size(_Δn_ϵ)), expecting $(N_int) x $(NC)"
+
+    ff = fermiFunctions(E_f, beta)
+    _ff_ϵ = ff.(ϵ_grid, beta)
+    
+    # indices n, m, p
+    f_rr = zeros(ComplexF64, NC, NC, NC, N_int)
+    f_rr .+= reshape(_gn_R_ϵ_pω1_pω2, NC, 1, 1, N_int)
+    f_rr .*= reshape(_gn_R_ϵ_pω2,     1, NC, 1, N_int)
+    f_rr .*= reshape(_Δn_ϵ,           1, 1, NC, N_int)
+
+    f_ar = zeros(ComplexF64, NC, NC, NC, N_int)
+    f_ar .+= reshape(_gn_R_ϵ_pω1, NC, 1, 1, N_int)
+    f_ar .*= reshape(_Δn_ϵ,       1, NC, 1, N_int)
+    f_ar .*= reshape(_gn_A_ϵ_mω2, 1, 1, NC, N_int)
+
+    f_aa = zeros(ComplexF64, NC, NC, NC, N_int)
+    f_aa .*= reshape(_Δn_ϵ,           NC, 1, 1, N_int)
+    f_aa .+= reshape(_gn_A_ϵ_mω1,     1, NC, 1, N_int)
+    f_aa .*= reshape(_gn_A_ϵ_mω1_mω2, 1, 1, NC, N_int)
+
+    return dropdims(sum((f_rr .+ f_ar .+ f_aa) .* reshape(_ff_ϵ, 1, 1, 1, N_int); dims=4); dims=4) * h
+end
+
+
+function gn_A(ϵ, n; λ=1e-10, δ=1e-5)
     # Equation 36 ctrl+k j3
     # λ is soft cutoff, δ is hard cutoff
     #if abs(1 - abs(ϵ)) < δ
@@ -81,7 +121,7 @@ function gn_A(ϵ; n, λ=1e-10, δ=1e-5)
     return numerator / denominator
 end
 
-function gn_R(ϵ; n, λ=1e-10, δ=1e-5)
+function gn_R(ϵ, n; λ=1e-10, δ=1e-5)
     # Equation 36 ctrl+k j3
     # λ is soft cutoff, δ is hard cutoff
     #if abs(1 - abs(ϵ)) < δ
@@ -92,7 +132,7 @@ function gn_R(ϵ; n, λ=1e-10, δ=1e-5)
     return numerator / denominator
 end
 
-function Δn(ϵ; n, δ=1e-5)
+function Δn(ϵ, n; δ=1e-5)
     # Equation 35 ctrl+k D*
     if abs(1 - abs(ϵ)) < δ
         return 0.0

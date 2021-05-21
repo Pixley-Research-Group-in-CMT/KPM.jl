c2ebf76a8b728d8b031e12a1e459044e45c81e5a
yxfu93 <yxfu93@hotmail.com>
2021-05-20 21:22:13 -0400

    longitudinal DC cond record all NC

--- src/applications/dc_cond_long.jl
+++ src/applications/dc_cond_long.jl
@@ -4,14 +4,17 @@ function dc_long(
                  H_rescale_factor,
                  NC::Int64, NR::Int64, NH::Int64;
                  verbose=0,
-                 psi_in=exp.(maybe_on_device_rand(dt_real, NH, NR) * (2.0im * pi)),
+                 psi_in=nothing,
                  kernel=KPM.JacksonKernel,
                  Ef=0.0,
                  # workspace kwargs
-                 ψr=maybe_on_device_zeros(dt_cplx, NH, NR),
-                 ψ0=maybe_on_device_zeros(dt_cplx, NH, NR),
-                 ψall_r=maybe_on_device_zeros(dt_cplx, NH, NR, 3),
+                 ψr=maybe_on_device_zeros(dt_cplx, NH, NR * 2),
+                 ψ0=maybe_on_device_zeros(dt_cplx, NH, NR * 2),
+                 ψall_r=maybe_on_device_zeros(dt_cplx, NH, NR * 2, 3),
                 )
+
+    cond = on_host_zeros(dt_cplx, NC)
+
     kernel_vec = maybe_to_device(kernel.(0:NC-1, NC))
     kernel_vec .*= hn.(0:NC-1)
 
@@ -20,7 +23,11 @@ function dc_long(
     Tn_e = chebyshevT_accurate.((1:NC) .- 1, Ef_tilde)
 
 
-    ψ0 .= maybe_to_device(psi_in)
+    if isnothing(psi_in)
+        psi_in = exp.(rand(Float64, H_norm.n, NR) * 2im * pi);
+        normalize_by_col(psi_in, NR)
+    end
+    psi_in = maybe_to_device(psi_in)
 
     H = maybe_to_device(H)
     Jα = maybe_to_device(Jα)
@@ -29,10 +36,12 @@ function dc_long(
     ψall_r_views = map(x -> view(ψall_r, :, :, x), 1:3)
 
     # right start
+    view(ψ0, :, 1:NR) .= psi_in
+    mul!(view(ψ0, :, (NR+1):(2*NR)), Jα, psi_in)
 
     # loop over r
     n = 1 # THIS IS g0, T0, etc.
-    mul!(ψall_r_views[r_i(n)], Jα, ψ0)
+    ψall_r_views[r_i(n)] .= ψ0
     ψr .+= ψall_r_views[r_i(n)] .* kernel_vec[n] .* Tn_e[n]
 
     n = 2
@@ -50,32 +59,11 @@ function dc_long(
                               ψall_r_views[r_ip(n)],
                               ψall_r_views[r_i(n)])
         ψr .+= ψall_r_views[r_i(n)] .* kernel_vec[n] .* Tn_e[n]
+
+        cond[n] = dot(view(ψr, :, 1:NR), Jα, view(ψr, :, (NR+1):(NR*2)))
     end
 
-
-    # loop over l
-    m = 1 # g0, T0, etc.
-    mul!(ψall_r_views[r_i(m)], Jα, ψr)
-    ψr .= 0
-    ψr .+= ψall_r_views[r_i(m)] .* kernel_vec[m] .* Tn_e[m]
-
-    m = 2
-    mul!(ψall_r_views[r_i(m)], H, ψall_r_views[r_ip(m)])
-    ψr .+= ψall_r_views[r_i(m)] .* kernel_vec[m] .* Tn_e[m]
-
-    m_enum = 3:NC
-    if verbose >= 1
-        println("loop over m=3:$(NC)")
-        m_enum = ProgressBar(m_enum)
-    end
-
-    for m in m_enum # TODO : save memory possible here. We do not need 3 vectors for psi 2
-        chebyshev_iter_single(H,
-                              ψall_r_views[r_ipp(m)],
-                              ψall_r_views[r_ip(m)],
-                              ψall_r_views[r_i(m)])
-        ψr .+= ψall_r_views[r_i(m)] .* kernel_vec[m] .* Tn_e[m]
-    end
-
-    return dot(ψr, ψ0) / H_rescale_factor
+    return cond / H_rescale_factor / NR
 end
+
+

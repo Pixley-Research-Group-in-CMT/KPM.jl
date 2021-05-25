c2839c02b5d37f1a83aab0ff5507944476a942e9
Yixing <yf160@physics.rutgers.edu>
2021-05-25 17:37:26 -0400

    allow gpu on 3d cpge

--- src/applications/cpge.jl
+++ src/applications/cpge.jl
@@ -79,6 +79,8 @@ function d_cpge(Gamma, NC, ω₁, ω₂; E_f=0.0, beta=Inf, δ=1e-5, λ=0.0, ker
     _ff_ϵ = ff.(ϵ_grid)
     @debug "$(ϵ_grid)"
 
+    Gamma = maybe_to_device(Gamma)
+
     f_rr = zeros(ComplexF64, NC, NC, NC)
     f_ar = zeros(ComplexF64, NC, NC, NC)
     f_aa = zeros(ComplexF64, NC, NC, NC)
@@ -94,6 +96,7 @@ function d_cpge(Gamma, NC, ω₁, ω₂, ϵ; δ=1e-5, λ=0.0, kernel=JacksonKern
                f_aa = zeros(ComplexF64, NC, NC, NC),
                gn_ϵ = zeros(ComplexF64, NC)
                )
+    Gamma = maybe_to_device(Gamma)
 
     @debug "calculating for ϵ=$(ϵ)"
 
@@ -139,7 +142,7 @@ function d_cpge(Gamma, NC, ω₁, ω₂, ϵ; δ=1e-5, λ=0.0, kernel=JacksonKern
     gn_ϵ .= gn_A.(ϵ .- ω₁ .- ω₂, n_grid, λ, δ)
     f_aa .*= reshape(gn_ϵ, 1, 1, NC)
 
-    res = sum((f_rr .+ f_ar .+ f_aa) .* Gamma)
+    res = sum((f_rr + f_ar + f_aa) .* Gamma)
     return res
 end
 

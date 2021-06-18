1584c06e5689891c7db861bccba3de28968f6fb5
Yixing <yf160@physics.rutgers.edu>
2021-06-18 12:27:41 -0400

    set dt all in device.jl

--- src/applications/dc_cond_long.jl
+++ src/applications/dc_cond_long.jl
@@ -37,7 +37,7 @@ function dc_long(
 
 
     if isnothing(psi_in)
-        psi_in = exp.(rand(Float64, H.n, NR) * 2im * pi);
+        psi_in = exp.(rand(dt_real, H.n, NR) * 2im * pi);
         normalize_by_col(psi_in, NR)
     end
     psi_in = maybe_to_device(psi_in)

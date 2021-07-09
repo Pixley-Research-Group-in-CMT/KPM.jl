56bcfdf35f8d3554385858b9af3d65c790c2ba1f
yxfu93 <yxfu93@hotmail.com>
2021-07-08 22:47:53 -0400

    not casting eltype; just warn

--- src/device.jl
+++ src/device.jl
@@ -13,45 +13,41 @@ function whichcore()
 end
 whichcore()
 
-function maybe_to_device(x::Union{SparseMatrixCSC, CUSPARSE.CuSparseMatrixCSC})
+
+function maybe_to_device(x::Union{SparseMatrixCSC, CUSPARSE.CuSparseMatrixCSC}, expect_eltype=dt_num)
+    if !(eltype(x) <: expect_eltype)
+        @warn "element type $(eltype(x)) is not in expect_eltype=$(expect_eltype). Not casting, though."
+    end
+
     if CUDA.has_cuda()
         if (typeof(x) <: CUDA.CUSPARSE.CuSparseMatrixCSC)
-            if (eltype(x) == dt_cplx)
-                return x
-            else
-                @info "Inefficient conversion of eltype ($(eltype(x)) to $(dt_cplx)) in $(typeof(x))"
-                return maybe_to_device(maybe_to_host(x))
-            end
-        else
-            return CUDA.CUSPARSE.CuSparseMatrixCSC{dt_cplx}(x)
-        end
-    else
-        if eltype(x) == dt_cplx
             return x
         else
-            return SparseMatrixCSC{dt_cplx, Int64}(x)
+            return CUDA.CUSPARSE.CuSparseMatrixCSC{eltype(x)}(x)
         end
+    else
+        return x
     end
 end
 
-function maybe_to_device(x::Union{Array, CuArray})
+function maybe_to_device(x::Union{Array, CuArray}, expect_eltype=dt_num)
+    if !(eltype(x) <: expect_eltype)
+        @warn "element type $(eltype(x)) is not in expect_eltype=$(expect_eltype). Not casting, though."
+    end
+
     if CUDA.has_cuda()# && eltype(x).isbitstype
-        if (typeof(x) <: CUDA.CuArray) && (eltype(x) == dt_cplx)
+        if (typeof(x) <: CUDA.CuArray)
             return x
         else
-            return CUDA.CuArray{dt_cplx}(x)
+            return CUDA.CuArray{eltype(x)}(x)
         end
     else
-        if eltype(x) == dt_cplx
-            return x
-        else
-            return Array{dt_cplx}(x)
-        end
+        return x
     end
 end
 
 
-maybe_to_device(x::SubArray) = x # Pushing SubArray to GPU is bad for current CUDA stack.
+maybe_to_device(x::SubArray, expect_eltype=dt_num) = x # Pushing SubArray to GPU is bad for current CUDA stack.
 
 maybe_to_host(x::Array) = x
 maybe_to_host(x::SparseMatrixCSC) = x

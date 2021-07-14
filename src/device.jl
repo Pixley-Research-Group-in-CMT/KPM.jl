3eeea426ec0bda21bbad01c8143c8c75ace3ac64
yxfu93 <yxfu93@hotmail.com>
2021-07-14 10:09:45 -0400

    tie workspace to MDCuArray; enable in device.jl

--- src/device.jl
+++ src/device.jl
@@ -14,32 +14,57 @@ end
 whichcore()
 
 
-function maybe_to_device(x::Union{SparseMatrixCSC, CUSPARSE.CuSparseMatrixCSC}, expect_eltype=dt_num)
+function maybe_to_device(x::Union{SparseMatrixCSC, CUSPARSE.CuSparseMatrixCSC}, expect_eltype=dt_num; multigpu=0)
     if !(eltype(x) <: expect_eltype)
         @warn "element type $(eltype(x)) is not in expect_eltype=$(expect_eltype). Not casting, though."
     end
 
     if CUDA.has_cuda()
-        if (typeof(x) <: CUDA.CUSPARSE.CuSparseMatrixCSC)
-            return x
+        if multigpu > 0
+            # Not already splitted, limited by function signature.
+            if typeof(x) <: CUSPARSE.CuSparseMatrixCSC
+                @info "Splitting $(typeof(x)). This is inefficient especially if it is already in GPU. Temporarily, we recommend to do it explicitly by `H = KPM._split_sparse_matrix(H; Ns=2)` for example."
+                x = maybe_to_host(x)
+            end
+
+            x_split = _split_sparse_matrix(x; Ns = multigpu)
+            return maybe_to_device(x_split)
+
         else
-            return CUDA.CUSPARSE.CuSparseMatrixCSC{eltype(x)}(x)
+            if (typeof(x) <: CUDA.CUSPARSE.CuSparseMatrixCSC)
+                return x
+            else
+                return CUDA.CUSPARSE.CuSparseMatrixCSC{eltype(x)}(x)
+            end
         end
     else
         return x
     end
 end
 
-function maybe_to_device(x::Union{Array, CuArray}, expect_eltype=dt_num)
+function maybe_to_device(x::OpsSplits)
+    @assert CUDA.has_cuda() "Do not split operators when not using GPU for now."
+    return _broadcast_operator_splits(x)
+end
+
+function maybe_to_device(x::Union{Array, CuArray}, expect_eltype=dt_num; multigpu=0, split_hint=nothing)
     if !(eltype(x) <: expect_eltype)
         @warn "element type $(eltype(x)) is not in expect_eltype=$(expect_eltype). Not casting, though."
     end
 
     if CUDA.has_cuda()# && eltype(x).isbitstype
-        if (typeof(x) <: CUDA.CuArray)
+        if multigpu > 0
+            if isnothing(split_hint)
+                @info "workspace not created when building unified memory array"
+            end
+            x = _create_UM_arr(x; split_hint=split_hint)
             return x
         else
-            return CUDA.CuArray{eltype(x)}(x)
+            if (typeof(x) <: CUDA.CuArray)
+                return x
+            else
+                return CUDA.CuArray{eltype(x)}(x)
+            end
         end
     else
         return x
--- src/utils/multigpu.jl
+++ src/utils/multigpu.jl
@@ -1,8 +1,9 @@
 ## A collection of functions to allow spreading out load across GPUs on sparse matrix × dense vector.
-
+using LinearAlgebra
 struct OpsSplits{T}
     Ns::Int64
     arrs::Array
+    split_list::Array{Int64, 1}
 end
 
 Base.eltype(x::OpsSplits{T}) where {T} = T
@@ -10,37 +11,64 @@ Base.eltype(x::OpsSplits{T}) where {T} = T
 struct MDCuSplits{T}
     Ns::Int64
     arrs::Array
+    split_list::Array{Int64, 1}
 end
 Base.eltype(x::MDCuSplits{T}) where {T} = T
 
 struct MDCuCopy{T}
     Ns::Int64
     arrs::Array
+    split_list::Array{Int64, 1}
 end
 Base.eltype(x::MDCuCopy{T}) where {T} = T
 
-function _create_UM_arr(a_h)
+struct MDCuArray{T}
+    arr::CuArray{T}
+    work_space::Union{MDCuCopy{T}, Nothing}
+end
+Base.eltype(x::MDCuArray{T}) where {T} = T
+Base.collect(x::MDCuArray) = collect(x.arr)
+
+function LinearAlgebra.Hermitian(x::MDCuSplits)
+    @warn "Hermitian is bypassed for MDCuSplits"
+    return x # do nothing when asking to be Hermitian
+end
+
+function _create_UM_arr(a_h; split_hint=nothing)
     et = eltype(a_h)
     @assert isbitstype(et) "element type of a_h, $(et), is not bits type."
     buf_a = Mem.alloc(Mem.Unified, sizeof(a_h))
     a_d = unsafe_wrap(CuArray{et}, convert(CuPtr{et}, buf_a), size(a_h))
     copyto!(a_d, a_h)
-    return a_d
+
+    work_space = nothing
+    if !isnothing(split_hint)
+        work_space = _prepare_workspace(a_d, split_hint)
+    end
+
+    return MDCuArray(a_d, work_space)
 end
 
 function _broadcast_operator_splits(As::OpsSplits)
     Ns = length(collect(devices()))
     et = eltype(As)
     @assert As.Ns==Ns "length of As is $(As.Ns); does not match Ns=$(Ns) that is given by device number."
+
     Ascu = Array{CUSPARSE.CuSparseMatrixCSC{et}}(undef, Ns)
     for (gpu, dev) in enumerate(devices())
         device!(dev)
         Ascu[gpu] = As.arrs[gpu]
     end
-    return MDCuSplits{et}(Ns, Ascu)
+    return MDCuSplits{et}(Ns, Ascu, As.split_list)
 end
 
-function _prepare_workspace(arr_out::CuArray)
+_prepare_workspace(arr_out::MDCuArray, As) = _prepare_workspace(arr_out.arr, As)
+function _prepare_workspace(arr_out::CuArray, As::Union{OpsSplits, MDCuSplits, Array{Int64, 1}})
+    if typeof(As) <: Array
+        split_list = As
+    else
+        split_list = As.split_list
+    end
     et = eltype(arr_out)
 
     Ns = length(collect(devices()))
@@ -48,26 +76,37 @@ function _prepare_workspace(arr_out::CuArray)
 
     for (gpu, dev) in enumerate(devices())
         device!(dev)
-        arr_w[gpu] = CUDA.zeros(et, N, NR)
+        arr_w[gpu] = CUDA.zeros(et, split_list[gpu], NR)
     end
 
     for (gpu, dev) in enumerate(devices())
         device!(dev)
         synchronize()
     end
-    return MDCuCopy{et}(Ns, arr_w)
+    return MDCuCopy{et}(Ns, arr_w, split_list)
 end
 
-function f!(Ascu::MDCuSplits, arr_in::CuArray, arr_out::CuArray; w::MDCuCopy=_prepare_workspace())
-    @. arr_out = 0
+function LinearAlgebra.mul!(Ascu::MDCuSplits, arr_in::MDCuArray, arr_out::MDCuArray; w::MDCuCopy=nothing)
+
+    if isnothing(w)
+        if !isnothing(arr_out.work_space)
+            w = arr_out.work_space
+        else
+            w = _prepare_workspace(arr_out.arr, Ascu)
+        end
+    end
+
+    lb, ub = _get_lb_ub(Ascu.split_list)
+    @. arr_out.arr = 0
     @sync for (gpu, dev) in enumerate(devices())
         device!(dev)
-        mul!(w.arrs[gpu], Ascu.arrs[gpu], arr_in)
+        mul!(w.arrs[gpu], Ascu.arrs[gpu], arr_in.arr)
     end
+
 
     for (gpu, dev) in enumerate(devices())
         device!(dev)
-        @sync @. arr_out += w.arrs[gpu]
+        @. arr_out.arr[lb[gpu]:ub[gpu], :] = w.arrs[gpu]
     end
     return nothing
 end
@@ -78,20 +117,14 @@ function _split_sparse_matrix(A; Ns=2)
     et = eltype(A)
     As = Array{SparseMatrixCSC{et}}(undef, Ns)
 
-    if ishermitian(A)
-        @assert (A.uplo == 'U') "only implemented for upper triangular representation of Hermitian"
-        A = sparse(UpperTriangular(A))
-    end
+
+    split_list = _partition_l(size(A, 1), Ns)
+    lb, ub = _get_lb_ub(split_list)
 
     for i = 1:Ns
-        A_tmp = copy(A)
-        A_tmp[:, i:Ns:end] .= 0 # set column is faster. Warning: Hermitian is lost! This splitting need to be taken care of inside make_operators.jl
-        dropzeros!(A_tmp)
-
-        if ishermitian(A)
-            A_tmp = Hermitian(A_tmp, :U)
-        end
-        As[i] = A_tmp
+        # set column is faster; but we want non square. Warning: Hermitian is lost! This splitting need to be taken care of inside make_operators.jl
+        As[i] = A[lb[i]:ub[i], :]
     end
-    return OpsSplits{et}(Ns, As)
+    return OpsSplits{et}(Ns, As, split_list)
 end
+
